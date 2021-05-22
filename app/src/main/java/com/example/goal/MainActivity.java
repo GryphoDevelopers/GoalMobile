a947c85cb3d0d98844b4cf300b98a07fe53bcd93
GuilhermePalma <guilherme.palma8@etec.sp.gov.br>
2021-05-22 19:42:17 -0300

    Alteracao na Main Activity e criacao de novas Telas

--- app/src/main/java/com/example/goal/MainActivity.java
+++ app/src/main/java/com/example/goal/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.goal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void login(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void singIn(View view){
+        Intent intent = new Intent(this, SingInActivity.class);
+        startActivity(intent);
+    }
+
+    public void nextStage(View view){
+       /* Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);*/
+
+    }
+
 }
