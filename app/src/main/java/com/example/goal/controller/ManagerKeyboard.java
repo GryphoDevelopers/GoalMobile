c30f93521dd9f35542b1a3585dc908f5efe237e2
GuilhermePalma <guilherme.palma8@etec.sp.gov.br>
2021-07-25 18:57:20 -0300

    Update: Alterações nas Variaveis e Metodos

--- app/src/main/java/com/example/goal/controller/ManagerKeyboard.java
+++ app/src/main/java/com/example/goal/controller/ManagerKeyboard.java
@@ -8,18 +8,18 @@ import android.view.inputmethod.InputMethodManager;
 
 public class ManagerKeyboard {
 
-    private Context context;
+    private final Context context;
+    private final InputMethodManager keyboardManager;
 
     public ManagerKeyboard(Context context){
         this.context = context;
+        // Obtem o estado do Keyboard
+        this.keyboardManager = (InputMethodManager) context.
+                getSystemService(Context.INPUT_METHOD_SERVICE);
     }
 
     // Abre o Teclado
     public void openKeyboard(View viewOpen){
-        // Obtem o estado do Keyboard
-        InputMethodManager keyboardManager = (InputMethodManager)
-                context.getSystemService(Context.INPUT_METHOD_SERVICE);
-
         // Se ontem o controlador do Teclado = Abre
         if(keyboardManager != null){
             keyboardManager.showSoftInput(viewOpen, InputMethodManager.SHOW_IMPLICIT);
@@ -28,10 +28,6 @@ public class ManagerKeyboard {
 
     // Fecha o Teclado
     public void closeKeyboard(Activity activity){
-        // Obtem o estado do Keyboard
-        InputMethodManager keyboardManager = (InputMethodManager)
-                context.getSystemService(Context.INPUT_METHOD_SERVICE);
-
         // Se obtem o controlador do Teclado = Fecha
         if(keyboardManager != null){
             keyboardManager.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(),
--- app/src/main/java/com/example/goal/views/SnackBarPersonalized.java
+++ app/src/main/java/com/example/goal/views/SnackBarPersonalized.java
@@ -1,28 +1,19 @@
 package com.example.goal.views;
 
-import android.content.Context;
 import android.view.View;
 
-import com.example.goal.R;
 import com.google.android.material.snackbar.Snackbar;
 
 public class SnackBarPersonalized {
 
-    private View view;
+    private final View view;
 
     public SnackBarPersonalized(View view){
         this.view = view;
     }
 
     public Snackbar makeDefaultSnackBar(int string_text){
-        Snackbar snackbar = Snackbar.make(view, string_text, Snackbar.LENGTH_LONG);
-        snackbar.setAction(R.string.button_close, v -> {
-            // Listener do Botão "Fechar"
-            snackbar.dismiss();
-        });
-
-        return snackbar;
+        return Snackbar.make(view, string_text, Snackbar.LENGTH_LONG);
     }
-
 
 }
